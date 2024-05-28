3cac04fe6dcaaa7d1a9b01fbc3f908f2c61c284a
Emmanuel Wangeci <123594129+EmmanuelWangeci@users.noreply.github.com>
2024-05-28 12:53:41 +0300

    Code to Send Contact info to google sheets

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,45 @@
+var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab")
+    }
+</script>
+<script>
 
+    var sidemeu = document.getElementById("sidemenu");
+
+    function openmenu(){
+        sidemeu.style.right = "0";
+    }
+    function closemenu(){
+        sidemeu.style.right = "-200px";
+    }
+</script>
+<script>
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyskNhzq_GWCARJo2wHUkc1epni7KfXpBoAM7ydsktwGMnUJjBXKPX9YONRo5g9WMO-5g/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => 
+        {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
     var icon = document.getElementById("icon")
     icon.onclick = function(){
         document.body.classList.toggle("light-theme")
